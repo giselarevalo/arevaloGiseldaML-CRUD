09628bcde51f71d75178a745961622c12d562e63
Giselda  Arevalo <96034020+giselarevalo@users.noreply.github.com>
2023-08-15 22:04:45 -0300

    update for products CRUD

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -8,10 +8,20 @@ const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 const controller = {
 	index: (req, res) => {
-		return res.render('index')
+		return res.render('index',{
+			visited : products.filter(product=> product.category === 'visited'),
+			sale: products.filter(product=> product.category === 'in-sale'),
+			toThousand
+		})
 	},
 	search: (req, res) => {
-		// Do the magic
+		const keywords = req.query.keywords
+		const results = products.filter(product=> product.name.toLowerCase().includes(keywords.toLowerCase()))
+		return res.render('results',{
+			results,
+			toThousand,
+			keywords
+		})
 	},
 };
 
